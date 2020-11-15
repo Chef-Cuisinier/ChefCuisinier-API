d088dc77ce54a6fc67957e33d371935b28438306
Quentin 'Torvald' LAURENT <ql2697@gmail.com>
2020-11-15 16:27:13 +0100

    :card_file_box: Created recipe entity

---
+++ src/recipe/recipe.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'recipe' })
+export class RecipeEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ type: 'int2' })
+  price: number;
+
+  @Column({ type: 'int2' })
+  difficulty: number;
+
+  @Column({ type: 'int4' })
+  preparationTime: number;
+
+  @Column({ type: 'int4' })
+  cookingTime: number;
+
+  @Column({ type: 'int4' })
+  restTime: number;
+
+  @Column({ type: 'timestamptz' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamptz' })
+  updatedAt: Date;
+}
